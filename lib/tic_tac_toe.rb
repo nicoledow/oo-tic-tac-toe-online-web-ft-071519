efbd8e01305b65ca6429fa31e69bf2cba1dc8483
Nicole Dow <nikki.dow71@gmail.com>
2019-07-24 23:26:48 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -94,17 +94,6 @@ class TicTacToe
   end
   
   # #winner returns token "X" or "O" that has won the game when given a winning board
-  # def winner
-  #   WIN_COMBINATIONS.detect do |combo|
-  #     if ((@board[combo[0]] == "X") && (@board[combo[1]] == "X") && (@board[combo[2]] == "X"))
-  #       return "X"
-  #     elsif ((@board[combo[0]] == "O") && (@board[combo[1]] == "O") && (@board[combo[2]] == "O"))
-  #       return "O"
-  #     else
-  #       nil
-  #     end
-  #   end
-  # end
   def winner
     won? ? board[won?[0]] : nil
   end
