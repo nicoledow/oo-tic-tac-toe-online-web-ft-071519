2306052914048ca96bb1fae02a4247943cff9ce3
Nicole Dow <nikki.dow71@gmail.com>
2019-07-24 23:22:50 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -69,8 +69,7 @@ class TicTacToe
   #won? returns false or nil if there is no win combination present, and returns the winning combination indices if there is a win
   def won?
     WIN_COMBINATIONS.detect do |combo|
-      if ((@board[combo[0]] == @board[combo[1]]) && (@board[combo[1]] == @board[combo[2]]) && (@board[combo[2]] == @board[combo[0]]))
-          combo
+      if ((@board[combo[0]] == @board[combo[1]]) && (@board[combo[1]] == @board[combo[2]]))
       else
         false
       end
